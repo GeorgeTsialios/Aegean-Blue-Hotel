b8f093d476f0101dfa327faca023769c7c2909b5
ΓΕΩΡΓΙΟΣ ΤΣΙΑΛΙΟΣ <up1072868@upnet.gr>
2023-05-17 19:58:58 +0300

    Booking Form page parameterized (without db)

---
+++ controllers/frontEndControllers/bookingFormController.mjs
@@ -0,0 +1,79 @@
+import { ApiControllers } from "../index.mjs";
+
+function navigateToBookingForm(req, res, next) {
+    try {
+        const account = ApiControllers.AccountController.returnAccount();
+        const hotel = ApiControllers.HotelController.returnHotel();
+        const checkInDate = new Date(req.query.checkInDate);
+        const checkOutDate = new Date(req.query.checkOutDate);
+        const lengthOfStayString = `${(checkOutDate -checkInDate) / (1000 * 3600 * 24)} ${(checkOutDate -checkInDate) / (1000 * 3600 * 24) === 1 ? "night" : "nights"}`;
+        const adultsCount = Number(req.query.adultsCount);
+        const childrenCount = Number(req.query.childrenCount);
+        const infantsCount = Number(req.query.infantsCount);
+        const guestsString = `${adultsCount} ${(adultsCount === 1) ? "adult" : "adults"} • ${childrenCount} ${(childrenCount === 1) ? "child" : "children"} • ${infantsCount} ${(infantsCount === 1) ? "infant" : "infants"}`;
+
+        const roomTypesForBooking = [];
+        const roomTypes = ApiControllers.RoomTypeController.returnRoomTypes();
+        roomTypes.forEach(roomType => {
+            let roomTypeCodeCount = `${roomType.code}Count`;
+            let roomTypeCount = Number(req.query[roomTypeCodeCount]);
+            if (roomTypeCount > 0)
+                roomTypesForBooking.push({
+                    code: roomType.code,
+                    name: roomType.name,
+                    count: roomTypeCount
+                });
+        });
+
+        const originalPrice =  !Number.isInteger(Number(req.query.originalPrice))? Number(req.query.originalPrice).toFixed(2) : Number(req.query.originalPrice);
+        const discount = !Number.isInteger(Number(req.query.discount))? Number(req.query.discount).toFixed(2) : Number(req.query.discount);
+        const totalPrice = !Number.isInteger(Number(req.query.totalPrice))? Number(req.query.totalPrice).toFixed(2) : Number(req.query.totalPrice);
+
+        const breakfastIncluded = req.query.breakfastIncluded === "true";
+        const freeCancellationIncluded = req.query.freeCancellationIncluded === "true";
+
+        res.render(
+            "bookingForm",
+            {
+                title: "Booking Form",
+                linkTags: `
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/intl-tel-input@18.1.1/build/css/intlTelInput.css">
+                <link rel="stylesheet" href="/css/booking_form.css">
+                <link rel="stylesheet" href="/css/countryselect.css">
+                `,
+                scriptTags: `
+                <script src="https://cdn.jsdelivr.net/npm/intl-tel-input@18.1.1/build/js/intlTelInput.min.js"></script>
+                <script src = "/js/booking_form.js"></script>
+                <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+                <script src="/js/countryselect.min.js"></script>
+                <script> $("#country").countrySelect({
+                    defaultCountry: "gr",
+                    preferredCountries: ["gr"],
+                    responsiveDropdown: true
+                    }); 
+                </script>
+                `,
+                account: account,
+                hotel: hotel,
+                checkInDate: checkInDate.toLocaleDateString("en-us", {"month": "short", "day": "numeric", "year": "numeric"}),
+                checkOutDate: checkOutDate.toLocaleDateString("en-us", {"month": "short", "day": "numeric", "year": "numeric"}),
+                lengthOfStayString: lengthOfStayString,
+                adultsCount: adultsCount,
+                childrenCount: childrenCount,
+                infantsCount: infantsCount,
+                guestsString: guestsString,
+                roomTypesForBooking: roomTypesForBooking,
+                breakfastIncluded: breakfastIncluded,
+                freeCancellationIncluded: freeCancellationIncluded,
+                originalPrice: originalPrice,
+                discount: discount,
+                totalPrice: totalPrice
+            }
+        );
+    }
+    catch (err) {
+        next(err);
+    }
+}
+
+export { navigateToBookingForm }
---
+++ public/js/booking_form.js
@@ -0,0 +1,161 @@
+const phone = document.querySelector("#phone");
+const phoneRegex = /^[0-9\(\)\-\s]+$/;
+const email = document.querySelector("#e-mail");
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const form = document.querySelector("form");
+// const streetNumber = document.querySelector("#street_number");
+// const postalCode = document.querySelector("#postal_code");
+// const NumberRegex = /^[0-9]+$/;
+const country = document.querySelector("#country");
+const phoneContainer = document.querySelector("#phone_container");
+
+let iti = null;
+
+let account = null;
+
+document.addEventListener("DOMContentLoaded",async () => {
+    await fetchAccount();
+    email.addEventListener('blur',validateEmail);
+    phone.addEventListener('blur',(event) => {
+      validatePhone(event);
+      phone.addEventListener('focus',validatePhone);
+    });
+    // streetNumber.addEventListener('blur',validateStreetNumber);
+    country.addEventListener('focus',() => country.style.color="black");
+
+    validate();
+    completeForm();
+    iti = window.intlTelInput(phone,{
+        utilsScript: "https://cdn.jsdelivr.net/npm/intl-tel-input@18.1.1/build/js/utils.js",
+        initialCountry: "gr",
+        preferredCountries: ["gr"],
+        separateDialCode: true,
+        geoIpLookup: callback => {
+        fetch("https://ipapi.co/json")
+          .then(res => res.json())
+          .then(data => callback(data.country_code))
+          .catch(() => callback("us"));
+        }
+      });
+
+    setPhoneDropdownSize();
+    window.addEventListener('resize',setPhoneDropdownSize);
+})
+
+const errorMap = {
+    "-99": "Please provide a valid phone number",
+    0:"Invalid number",
+    1:"Invalid country code",
+    2:"Too short",
+    3:"Too long",
+    4:"Invalid number"
+};
+
+async function fetchAccount() {
+  const response = await fetch(`/api/account/${accountEmail}`);
+  account = await response.json();
+}
+
+function validate() {
+  form.addEventListener('submit', (event) => {
+
+    email.addEventListener("keyup",validateEmail);
+    phone.addEventListener("keyup",validatePhone);
+    // streetNumber.addEventListener("keyup",validateStreetNumber);
+
+    if (!form.checkValidity()) {
+        event.preventDefault();
+        event.stopPropagation();
+    }
+
+    validateEmail(event);
+    validatePhone(event);
+    // validateStreetNumber(event);
+  
+    form.classList.add('was-validated');
+
+    let invalidInputs = document.querySelectorAll(".is-invalid");
+    if (invalidInputs.length === 0 && form.checkValidity())
+        submitForm();
+
+  }, false);
+}
+
+function validateEmail(event) {
+  if (!emailRegex.test(email.value)) {
+      email.setCustomValidity("E-mail is not valid");
+      email.classList.remove("is-valid");
+      email.classList.add("is-invalid");
+      event.preventDefault();
+      event.stopPropagation();
+  }
+  else {
+      email.setCustomValidity("");
+      email.classList.remove("is-invalid");
+      email.classList.add("is-valid");
+  }
+}
+
+function validatePhone(event) {
+    console.log(iti.getNumber());
+      if (iti.isValidNumber() && phoneRegex.test(phone.value)) {
+        phone.setCustomValidity("");
+        phone.parentElement.classList.remove("is-invalid");
+        phone.parentElement.classList.add("is-valid");
+        phone.classList.remove("is-invalid");
+        phone.classList.add("is-valid");
+      } 
+      else {
+        phone.setCustomValidity("Phone is not valid");
+        phone.parentElement.classList.remove("is-valid");
+        phone.parentElement.classList.add("is-invalid");
+        phone.classList.remove("is-valid");
+        phone.classList.add("is-invalid");
+        const errorCode = iti.getValidationError();
+
+        document.querySelector("#phone_error_message").textContent = `${errorMap[errorCode]}`;
+        event.preventDefault();
+        event.stopPropagation();
+      }
+}
+
+// function validateStreetNumber(event) {
+//     if (streetNumber.value === "")
+//         streetNumber.classList.add("optional");
+//     else {
+//         streetNumber.classList.remove("optional");
+//         if (!NumberRegex.test(streetNumber.value)) {
+//             streetNumber.setCustomValidity("Street number is not valid");
+//             streetNumber.classList.remove("is-valid");
+//             streetNumber.classList.add("is-invalid");
+//             event.preventDefault();
+//             event.stopPropagation();
+//         }
+//         else {
+//             streetNumber.setCustomValidity("");
+//             streetNumber.classList.remove("is-invalid");
+//             streetNumber.classList.add("is-valid");
+//         }
+
+//     }
+// }
+
+function submitForm() {
+    console.log("Send form");
+    phone.value = iti.getNumber();
+}
+
+function setPhoneDropdownSize() {
+    const PhoneDropdown = document.querySelector("#iti-0__country-listbox");
+    PhoneDropdown.style.width = `${phoneContainer.offsetWidth}px`;
+}
+
+function completeForm() {
+    if (account) {
+        document.querySelector("#fname").value = account.firstName;
+        document.querySelector("#lname").value = account.lastName;
+        email.value = account.email;
+        if (account.phoneNumber)
+            phone.value = account.phoneNumber;
+    }
+}
